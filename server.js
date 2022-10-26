bfeec621fab66b7fbea83bb44014b61e75b5e89a
gyoyeon Kim <gy2121@naver.com>
2022-10-26 23:45:51 +0900

    create model end

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true, useUnifiedTopology : true })
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false}))
+
+
 
 
 app.get('/',(req,res) => {
@@ -20,5 +26,7 @@ app.get('/',(req,res) => {
     }]
     res.render('articles/index', {articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
