e125377e10089a1c2af183bddae0d6d2e333d23e
gyoyeon Kim <gy2121@naver.com>
2022-10-27 00:45:29 +0900

    create delete form end

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', { 
     useNewUrlParser: true, useUnifiedTopology : true})
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
 
 
 
